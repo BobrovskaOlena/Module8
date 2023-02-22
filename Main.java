895002ff382dcc9ef7e7e90f2d743f461675632e
BobrovskaOlena <lena69114@gmail.com>
2023-02-22 22:37:39 +0200

    printName

--- Main.java
+++ Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Shape a = new Shape();
         a.printName();
+        Circle c = new Circle();
+        c.printName();
+        Quad q = new Quad();
+        q.printName();
+        Rectangle r = new Rectangle();
+        r.printName();
+        Triangle t = new Triangle();
+        t.printName();
+        Trapeze tr = new Trapeze();
+        tr.printName();
     }
 }
